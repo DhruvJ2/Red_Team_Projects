1512aeb175759e5a15fc62183de0e31599661569
Dhruv Jain <dhruvjain136@gmail.com>
2022-11-04 14:53:52 +0530

    Add Rot13

---
+++ 7_ROT13_cipher/rot13.py
@@ -0,0 +1,8 @@
+
+rot13 = str.maketrans(
+    'ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz',
+    'NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm')
+
+text = input('Enter text : \n> ')
+print("Your cipher text is: ")
+print(text.translate(rot13))
